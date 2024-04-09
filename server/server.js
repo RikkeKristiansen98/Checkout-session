9d124ace03e8b9fe2f342cfdd75ffd3f6ac6c32a
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2024-04-09 17:36:02 +0200

    login

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require ("express")
+const cookieSession = require("cookie-session")
+
 const userRouter = require("./resources/users.router")
 const authRouter = require("./resources/auth.router")
 
@@ -6,6 +8,10 @@ const authRouter = require("./resources/auth.router")
 const app = express()
 
 app.use(express.json())
+app.use(cookieSession({
+    secret: "r2kr3j",
+    maxAge: 1000 * 60 * 60 * 24,
+}))
 
 //Routes
 app.use("/api/users", userRouter);
